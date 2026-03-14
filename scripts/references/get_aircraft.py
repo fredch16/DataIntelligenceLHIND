3a6a8ceba19d56016167fe303b67fe52746147ef
fredch16 <frederickcharbonnier@icloud.com>
2026-03-14 20:14:06 +0000

    Resolved an issue with directory fetching

--- scripts/references/get_aircraft.py
+++ scripts/references/get_aircraft.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import time
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '../..')))
 from utils.helpers import LufthansaClient
 from datetime import datetime
 
