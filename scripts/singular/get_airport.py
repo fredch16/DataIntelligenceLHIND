124a6163733f5b7a599c5198487cf1af83076680
Fred at Level3 <frederickcharbonnier@icloud.com>
2026-03-17 15:55:48 +0100

    Testing secrets databricks

--- scripts/singular/get_airport.py
+++ scripts/singular/get_airport.py
@@ -2,12 +2,12 @@ import requests
 
 # Constants
 BASE_URL = "https://api.lufthansa.com"
-ACCESS_TOKEN = ""  # Replace with your actual token
+ACCESS_TOKEN = "ub24pf6mru8y534f3s4uhevx"  # Replace with your actual token
 
 # Define the endpoint and parameters
 endpoint = "/v1/references/airports?limit=1&offset=1543&lang=EN"
 url = f"{BASE_URL}{endpoint}"
-
+# ub24pf6mru8y534f3s4uhevx
 # Setup query parameters
 # params = {
 # 	"limit": 1,
@@ -22,6 +22,18 @@ headers = {
 }
 
 try:
+	scope = lufthansa_app_own
+	if "DATABRICKS_RUNTIME_VERSION" in os.environ:
+		from pyspark.sql import SparkSession
+		from pyspark.dbutils import DBUtils
+		spark = SparkSession.builder.getOrCreate()
+		dbutils = DBUtils(spark)
+		ACCESS_TOKEN = dbutils.secrets.get(scope=scope, key="access_token")
+	headers = {
+	"Authorization": f"Bearer {ACCESS_TOKEN}",
+	"Accept": "application/json"
+}
+
 	print(headers)
 	response = requests.get(url, headers=headers)
 	# response = requests.get(url, headers=headers, params=params)
